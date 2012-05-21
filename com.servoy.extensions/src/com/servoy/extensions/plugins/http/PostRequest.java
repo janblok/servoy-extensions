33e24687acc53f52cc8820b97e2cece3373bee67
Gabi Boros <gboros@servoy.com>
2012-05-21 14:46:34 +0000

    fix : method addFile of http plugin doesn't send any File Case: SVY-2168

--- com.servoy.extensions/src/com/servoy/extensions/plugins/http/PostRequest.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/http/PostRequest.java
@@ -143,7 +143,7 @@ public class PostRequest extends BaseEntityEnclosingRequest
 				}
 				content = null;
 			}
-			else if (files.size() == 1)
+			else if (files.size() == 1 && (params == null || params.size() == 0))
 			{
 				File f = files.values().iterator().next();
 				entity = new FileEntity(f, "binary/octet-stream");
