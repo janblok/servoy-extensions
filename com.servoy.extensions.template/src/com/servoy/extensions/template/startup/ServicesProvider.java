1792a80ae512151b0a61fb9ccf59705e4a8eec58
Johan Compagner <jcompagner@servoy.com>
2014-03-03 12:38:13 +0000

    rename of IServiceProvider to IServicesProvider

--- com.servoy.extensions.template/src/com/servoy/extensions/template/startup/ServicesProvider.java
+++ com.servoy.extensions.template/src/com/servoy/extensions/template/startup/ServicesProvider.java
@@ -3,11 +3,11 @@ package com.servoy.extensions.template.startup;
 import java.util.Set;
 
 import org.apache.tomcat.starter.ClassServiceFactory;
-import org.apache.tomcat.starter.IServiceProvider;
+import org.apache.tomcat.starter.IServicesProvider;
 
 import com.servoy.extensions.template.WebdavServlet;
 
-public class ServiceProvider implements IServiceProvider {
+public class ServicesProvider implements IServicesProvider {
 
 	public void registerServices() {
 		Activator.getContext().registerService(WebdavServlet.class.getName(), new ClassServiceFactory(WebdavServlet.class), null);
