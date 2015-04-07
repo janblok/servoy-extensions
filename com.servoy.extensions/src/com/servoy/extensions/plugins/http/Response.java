f2a3a5e7e49a024f88a2c0f2e7c1512a2deba1bf
emera <emera@servoy.com>
2015-04-07 10:56:41 +0300

    SVY-7977 Function "getBodyContent" on the HTTP reponse object can only be executed once (cache the response)

--- com.servoy.extensions/src/com/servoy/extensions/plugins/http/Response.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/http/Response.java
@@ -46,10 +46,11 @@ import com.servoy.j2db.util.Utils;
 public class Response implements IScriptable, IJavaScriptType
 {
 	private HttpResponse res;
+	private Object response_body = null;
 
 	public Response()
 	{
-
+		
 	}
 
 	public Response(HttpResponse response)
@@ -96,15 +97,19 @@ public class Response implements IScriptable, IJavaScriptType
 	 */
 	public String js_getResponseBody()
 	{
-		try
+		if (response_body == null)
 		{
-			return EntityUtils.toString(res.getEntity());
+			try
+			{
+				response_body = EntityUtils.toString(res.getEntity());
+			}
+			catch (Exception e)
+			{
+				Debug.error(e);
+				response_body = "";
+			}
 		}
-		catch (Exception e)
-		{
-			Debug.error(e);
-		}
-		return "";
+		return response_body instanceof String ? (String)response_body : "";
 	}
 
 	/**
@@ -115,28 +120,31 @@ public class Response implements IScriptable, IJavaScriptType
 	 */
 	public byte[] js_getMediaData()
 	{
-		try
+		if (response_body == null)
 		{
-			ByteArrayOutputStream sb = new ByteArrayOutputStream();
-			InputStream is = null;
-			Header contentEncoding = res.getFirstHeader("Content-Encoding");
-			boolean gziped = contentEncoding == null ? false : "gzip".equalsIgnoreCase(contentEncoding.getValue());
-			is = res.getEntity().getContent();
-			if (gziped)
+			try
 			{
-				is = new GZIPInputStream(is);
+				ByteArrayOutputStream sb = new ByteArrayOutputStream();
+				InputStream is = null;
+				Header contentEncoding = res.getFirstHeader("Content-Encoding");
+				boolean gziped = contentEncoding == null ? false : "gzip".equalsIgnoreCase(contentEncoding.getValue());
+				is = res.getEntity().getContent();
+				if (gziped)
+				{
+					is = new GZIPInputStream(is);
+				}
+				BufferedInputStream bis = new BufferedInputStream(is);
+				Utils.streamCopy(bis, sb);
+				bis.close();
+				is.close();
+				response_body = sb.toByteArray();
 			}
-			BufferedInputStream bis = new BufferedInputStream(is);
-			Utils.streamCopy(bis, sb);
-			bis.close();
-			is.close();
-			return sb.toByteArray();
+			catch (IOException e)
+			{
+				Debug.error(e);
+			}
 		}
-		catch (IOException e)
-		{
-			Debug.error(e);
-		}
-		return null;
+		return response_body instanceof byte[] ? (byte[])response_body : null;
 	}
 
 	/**
