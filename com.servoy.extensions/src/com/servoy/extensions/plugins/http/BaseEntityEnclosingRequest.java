1bcca1628be79a92caea9e974ad1e6de68b0f508
Rob Gansevles <rgansevles@gmail.com>
2015-09-11 13:46:50 +0200

    SVY-8179 http plug to PUT with params

--- com.servoy.extensions/src/com/servoy/extensions/plugins/http/BaseEntityEnclosingRequest.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/http/BaseEntityEnclosingRequest.java
@@ -17,13 +17,36 @@
 
 package com.servoy.extensions.plugins.http;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 
+import com.servoy.extensions.plugins.file.JSFile;
 import com.servoy.j2db.plugins.IClientPluginAccess;
+import com.servoy.j2db.util.Debug;
+import com.servoy.j2db.util.Pair;
 import com.servoy.j2db.util.Utils;
 
 /**
@@ -35,6 +58,10 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	private String mimeType;
 	protected String charset = HTTP.UTF_8;
 
+	private Map<Pair<String, String>, Object> files;
+	private List<NameValuePair> params;
+
+
 	public BaseEntityEnclosingRequest()
 	{
 	}
@@ -42,6 +69,12 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	public BaseEntityEnclosingRequest(String url, DefaultHttpClient hc, HttpRequestBase method, IClientPluginAccess plugin)
 	{
 		super(url, hc, method, plugin);
+		clearFiles();
+	}
+
+	protected final void clearFiles()
+	{
+		files = new HashMap<Pair<String, String>, Object>();
 	}
 
 	/**
@@ -50,7 +83,7 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	 * @sample
 	 * method.setBodyContent(content)
 	 *
-	 * @param content 
+	 * @param content
 	 */
 	public void js_setBodyContent(String content)
 	{
@@ -64,7 +97,7 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	 * method.setBodyContent(content, 'text/xml')
 	 *
 	 * @param content
-	 * @param mimeType 
+	 * @param mimeType
 	 */
 	public void js_setBodyContent(String content, String mimeType)
 	{
@@ -84,7 +117,7 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	 * poster.setCharset('UTF-8');
 	 * var httpCode = poster.executeRequest(scopes.globals.twitterUserName, scopes.globals.twitterPassword).getStatusCode() // httpCode 200 is ok
 	 *
-	 * @param charset 
+	 * @param charset
 	 */
 	public void js_setCharset(String s)
 	{
@@ -94,12 +127,190 @@ public class BaseEntityEnclosingRequest extends BaseRequest
 	@Override
 	protected HttpEntity buildEntity() throws Exception
 	{
-		if (!Utils.stringIsEmpty(content))
+		HttpEntity entity = null;
+		if (files.size() == 0)
 		{
-			StringEntity se = new StringEntity(content, mimeType, charset);
-			content = null;
-			return se;
+			if (params != null)
+			{
+				entity = new UrlEncodedFormEntity(params, charset);
+			}
+			else if (!Utils.stringIsEmpty(content))
+			{
+				entity = new StringEntity(content, mimeType, charset);
+				content = null;
+			}
 		}
-		return null;
+		else if (files.size() == 1 && (params == null || params.size() == 0))
+		{
+			Object f = files.values().iterator().next();
+			if (f instanceof File)
+			{
+				entity = new FileEntity((File)f, ContentType.create("binary/octet-stream")); //$NON-NLS-1$
+			}
+			else if (f instanceof JSFile)
+			{
+				entity = new InputStreamEntity(((JSFile)f).getAbstractFile().getInputStream(), ((JSFile)f).js_size(), ContentType.create("binary/octet-stream")); //$NON-NLS-1$
+			}
+			else
+			{
+				Debug.error("could not add file to post request unknown type: " + f);
+			}
+		}
+		else
+		{
+			entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+			// For File parameters
+			for (Entry<Pair<String, String>, Object> e : files.entrySet())
+			{
+				Object file = e.getValue();
+				if (file instanceof File)
+				{
+					((MultipartEntity)entity).addPart(e.getKey().getLeft(), new FileBody((File)file));
+				}
+				else if (file instanceof JSFile)
+				{
+					((MultipartEntity)entity).addPart(e.getKey().getLeft(),
+						new ByteArrayBody(Utils.getBytesFromInputStream(((JSFile)file).getAbstractFile().getInputStream()), "binary/octet-stream",
+							((JSFile)file).js_getName()));
+				}
+				else
+				{
+					Debug.error("could not add file to post request unknown type: " + file);
+				}
+			}
+
+			// add the parameters
+			if (params != null)
+			{
+				Iterator<NameValuePair> it = params.iterator();
+				while (it.hasNext())
+				{
+					NameValuePair nvp = it.next();
+					// For usual String parameters
+					((MultipartEntity)entity).addPart(nvp.getName(), new StringBody(nvp.getValue(), "text/plain", Charset.forName(charset)));
+				}
+			}
+		}
+
+		// entity may have been set already, see PutRequest.js_setFile
+		return entity;
 	}
+
+
+	/**
+	 * Add a file to the post.
+	 *
+	 * @sample
+	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
+	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
+	 *
+	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
+	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
+	 *
+	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
+	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
+	 *
+	 * @param parameterName
+	 * @param fileName
+	 * @param fileLocation
+	 */
+	public boolean js_addFile(String parameterName, String fileName, String fileLocation)
+	{
+		if (fileLocation != null)
+		{
+			File f = new File(fileLocation);
+			if (f.exists())
+			{
+				files.put(new Pair<String, String>(parameterName, fileName), f);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Add a file to the post.
+	 *
+	 * @sample
+	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
+	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
+	 *
+	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
+	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
+	 *
+	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
+	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
+	 *
+	 * @param parameterName
+	 * @param jsFile
+	 */
+	public boolean js_addFile(String parameterName, Object jsFile)
+	{
+		if (jsFile instanceof JSFile && ((JSFile)jsFile).js_exists())
+		{
+			files.put(new Pair<String, String>(parameterName, ((JSFile)jsFile).js_getName()), jsFile);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Add a file to the post.
+	 *
+	 * @sample
+	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
+	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
+	 *
+	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
+	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
+	 *
+	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
+	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
+	 *
+	 * @param parameterName
+	 * @param fileName
+	 * @param jsFile
+	 */
+	public boolean js_addFile(String parameterName, String fileName, Object jsFile)
+	{
+		if (jsFile instanceof JSFile && ((JSFile)jsFile).js_exists())
+		{
+			files.put(new Pair<String, String>(parameterName, fileName), jsFile);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Add a parameter to the post.
+	 *
+	 * @sample
+	 * poster.addParameter('name','value')
+	 * poster.addParameter(null,'value') //sets the content to post
+	 *
+	 * @param name
+	 * @param value
+	 */
+	public boolean js_addParameter(String name, String value)
+	{
+		if (name != null)
+		{
+			if (params == null)
+			{
+				params = new ArrayList<NameValuePair>();
+			}
+			params.add(new BasicNameValuePair(name, value));
+			return true;
+		}
+
+		if (value != null)
+		{
+			js_setBodyContent(value);
+			return true;
+		}
+
+		return false;
+	}
+
 }
--- com.servoy.extensions/src/com/servoy/extensions/plugins/http/PostRequest.java
+++ com.servoy.extensions/src/com/servoy/extensions/plugins/http/PostRequest.java
@@ -16,39 +16,15 @@
  */
 package com.servoy.extensions.plugins.http;
 
-import java.io.File;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.entity.InputStreamEntity;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ByteArrayBody;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.util.EntityUtils;
 
-import com.servoy.extensions.plugins.file.JSFile;
 import com.servoy.j2db.documentation.ServoyDocumented;
 import com.servoy.j2db.plugins.IClientPluginAccess;
 import com.servoy.j2db.util.Debug;
-import com.servoy.j2db.util.Pair;
-import com.servoy.j2db.util.Utils;
 
 /**
  * @author jblok
@@ -56,9 +32,6 @@ import com.servoy.j2db.util.Utils;
 @ServoyDocumented
 public class PostRequest extends BaseEntityEnclosingRequest
 {
-	private Map<Pair<String, String>, Object> files;
-	private List<NameValuePair> params;
-
 	public PostRequest()
 	{
 		super();
@@ -67,7 +40,6 @@ public class PostRequest extends BaseEntityEnclosingRequest
 	public PostRequest(String url, DefaultHttpClient hc, IClientPluginAccess plugin)
 	{
 		super(url, hc, new HttpPost(url), plugin);
-		files = new HashMap<Pair<String, String>, Object>();
 	}
 
 	/**
@@ -79,118 +51,6 @@ public class PostRequest extends BaseEntityEnclosingRequest
 		js_setCharset(encoding);
 	}
 
-	/**
-	 * Add a file to the post.
-	 *
-	 * @sample
-	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
-	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
-	 *
-	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
-	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
-	 *
-	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
-	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
-	 *
-	 * @param parameterName
-	 * @param fileName
-	 * @param fileLocation
-	 */
-	public boolean js_addFile(String parameterName, String fileName, String fileLocation)
-	{
-		if (fileLocation != null)
-		{
-			File f = new File(fileLocation);
-			if (f.exists())
-			{
-				files.put(new Pair<String, String>(parameterName, fileName), f);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * Add a file to the post.
-	 *
-	 * @sample
-	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
-	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
-	 *
-	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
-	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
-	 *
-	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
-	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
-	 *
-	 * @param parameterName
-	 * @param jsFile
-	 */
-	public boolean js_addFile(String parameterName, Object jsFile)
-	{
-		if (jsFile instanceof JSFile && ((JSFile)jsFile).js_exists())
-		{
-			files.put(new Pair<String, String>(parameterName, ((JSFile)jsFile).js_getName()), jsFile);
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Add a file to the post.
-	 *
-	 * @sample
-	 * poster.addFile('myFileParamName','manual.doc','c:/temp/manual_01a.doc')
-	 * poster.addFile(null,'postXml.xml','c:/temp/postXml.xml') // sets the xml to post
-	 *
-	 * var f = plugins.file.convertToJSFile('./somefile02.txt')
-	 * if (f && f.exists()) poster.addFile('myTxtFileParamName','somefile.txt', f)
-	 *
-	 * f = plugins.file.convertToJSFile('./anotherfile_v2b.txt')
-	 * if (f && f.exists()) poster.addFile('myOtherTxtFileParamName', f)
-	 *
-	 * @param parameterName
-	 * @param fileName
-	 * @param jsFile
-	 */
-	public boolean js_addFile(String parameterName, String fileName, Object jsFile)
-	{
-		if (jsFile instanceof JSFile && ((JSFile)jsFile).js_exists())
-		{
-			files.put(new Pair<String, String>(parameterName, fileName), jsFile);
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Add a parameter to the post.
-	 *
-	 * @sample
-	 * poster.addParameter('name','value')
-	 * poster.addParameter(null,'value') //sets the content to post
-	 *
-	 * @param name
-	 * @param value
-	 */
-	public boolean js_addParameter(String name, String value)
-	{
-		if (name != null)
-		{
-			if (params == null)
-			{
-				params = new ArrayList<NameValuePair>();
-			}
-			params.add(new BasicNameValuePair(name, value));
-			return true;
-		}
-		else if (value != null)
-		{
-			js_setBodyContent(value);
-			return true;
-		}
-		return false;
-	}
 
 	/**
 	 * @deprecated Replaced by {@link #executeRequest(String,String)}
@@ -225,75 +85,6 @@ public class PostRequest extends BaseEntityEnclosingRequest
 		}
 	}
 
-	@Override
-	protected HttpEntity buildEntity() throws Exception
-	{
-		HttpEntity entity = null;
-		if (files.size() == 0)
-		{
-			if (params != null)
-			{
-				entity = new UrlEncodedFormEntity(params, charset);
-			}
-			else
-			{
-				entity = super.buildEntity();
-			}
-		}
-		else if (files.size() == 1 && (params == null || params.size() == 0))
-		{
-			Object f = files.values().iterator().next();
-			if (f instanceof File)
-			{
-				entity = new FileEntity((File)f, ContentType.create("binary/octet-stream")); //$NON-NLS-1$
-			}
-			else if (f instanceof JSFile)
-			{
-				entity = new InputStreamEntity(((JSFile)f).getAbstractFile().getInputStream(), ((JSFile)f).js_size(), ContentType.create("binary/octet-stream")); //$NON-NLS-1$
-			}
-			else
-			{
-				Debug.error("could not add file to post request unknown type: " + f);
-			}
-		}
-		else
-		{
-			entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-
-			// For File parameters
-			for (Entry<Pair<String, String>, Object> e : files.entrySet())
-			{
-				Object file = e.getValue();
-				if (file instanceof File)
-				{
-					((MultipartEntity)entity).addPart(e.getKey().getLeft(), new FileBody((File)file));
-				}
-				else if (file instanceof JSFile)
-				{
-					((MultipartEntity)entity).addPart(e.getKey().getLeft(),
-						new ByteArrayBody(Utils.getBytesFromInputStream(((JSFile)file).getAbstractFile().getInputStream()), "binary/octet-stream",
-							((JSFile)file).js_getName()));
-				}
-				else
-				{
-					Debug.error("could not add file to post request unknown type: " + file);
-				}
-			}
-
-			// add the parameters
-			if (params != null)
-			{
-				Iterator<NameValuePair> it = params.iterator();
-				while (it.hasNext())
-				{
-					NameValuePair nvp = it.next();
-					// For usual String parameters
-					((MultipartEntity)entity).addPart(nvp.getName(), new StringBody(nvp.getValue(), "text/plain", Charset.forName(charset)));
-				}
-			}
-		}
-		return entity;
-	}
 
 	/**
 	 * Get the result page data after a post.
